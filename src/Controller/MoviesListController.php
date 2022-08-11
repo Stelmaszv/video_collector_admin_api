7ba3caefc5d7a0f07b6af8d5d8340b73219d026c
Stelmaszv <stelmaszv@gmail.com>
2022-08-11 13:17:06 +0200

    Add templete for MoviesDeteils

--- src/Controller/MoviesListController.php
+++ src/Controller/MoviesListController.php
@@ -5,13 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Generic\GenericSetDataInterFace;
+use App\Generic\GenericListController;
+use App\Entity\Movies;
 
-class MoviesListController extends AbstractController
+class MoviesListController extends GenericListController implements GenericSetDataInterFace
 {
-    public function listView(): Response
+    protected bool $paginate = TRUE;
+    protected int $per_page = 20;
+    public function setData(): void
     {
-        return $this->render('movies_list/index.html.twig', [
-            'controller_name' => 'MoviesListController',
-        ]);
+        $this->setEntity(Movies::class);
+        $this->setTwig('show/movies/list.html.twig');
     }
+
+    protected function onSetAttribut() :array
+    {
+        return  [
+            'Collector' => $this->returnUrlArguments('collector')
+        ];
+    }
+
 }
