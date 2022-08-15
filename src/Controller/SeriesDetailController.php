ead0c2e83a50245ba878234648f9c0bac9946e0d
Stelmaszv <stelmaszv@gmail.com>
2022-08-15 14:30:28 +0200

    Update SeriesDetailController

--- src/Controller/SeriesDetailController.php
+++ src/Controller/SeriesDetailController.php
@@ -5,18 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Generic\GenericDetailController;
 use App\Generic\GenericSetDataInterFace;
-use App\Generic\GenericListController;
 use App\Entity\Series;
 
-class SeriesListController extends GenericListController implements GenericSetDataInterFace
+class SeriesDetailController  extends GenericDetailController implements GenericSetDataInterFace
 {
-    protected bool $paginate = TRUE;
-    protected int $per_page = 20;
     public function setData(): void
     {
         $this->setEntity(Series::class);
-        $this->setTwig('show/series/list.html.twig');
+        $this->setTwig('show/series/detail.twig');
     }
 
     protected function onSetAttribut() :array
